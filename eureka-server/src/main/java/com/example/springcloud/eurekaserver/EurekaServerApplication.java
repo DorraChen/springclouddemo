c32c1c01d6dcb8ccb1d9c05a05e253e5d3419fac
clz <1814786186@qq.com>
2019-02-25 18:04:00 +0800

    新建一个Eureka Client

--- eureka-server/src/main/java/com/example/springcloud/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/springcloud/eurekaserver/EurekaServerApplication.java
@@ -2,7 +2,13 @@ package com.example.springcloud.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author admin
+ * @date 2019-02-25
+ */
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
