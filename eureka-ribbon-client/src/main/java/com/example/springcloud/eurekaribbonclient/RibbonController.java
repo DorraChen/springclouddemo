40a4fad1ce22f4d28a263a58668e3fe279c361bf
clz <1814786186@qq.com>
2019-02-28 09:57:53 +0800

    eureka-ribbon-client模块新增接口测试LoadBalancerClient

--- eureka-ribbon-client/src/main/java/com/example/springcloud/eurekaribbonclient/RibbonController.java
+++ eureka-ribbon-client/src/main/java/com/example/springcloud/eurekaribbonclient/RibbonController.java
@@ -1,6 +1,8 @@
 package com.example.springcloud.eurekaribbonclient;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +20,14 @@ public class RibbonController {
     @GetMapping("hi")
     public String hi(@RequestParam(required = false,defaultValue = "springclouddemo") String name){
         return restTemplate.getForObject("http://eureka-client/hi?name=" + name, String.class);
+    }
+
+    @Autowired
+    private LoadBalancerClient loadBalancer;
+
+    @GetMapping("testribbon")
+    public String testRibbon(){
+        ServiceInstance instance = loadBalancer.choose("eureka-client");
+        return instance.getHost() + ":" + instance.getPort();
     }
 }
