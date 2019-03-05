d38d30009079cf8e636cea11e6feaea412e2f198
clz <1814786186@qq.com>
2019-03-05 14:53:32 +0800

    开启Hystrix Dashboard监控Hystrix

--- eureka-feign-client/src/main/java/com/example/springcloud/eurekafeignclient/EurekaFeignClientApplication.java
+++ eureka-feign-client/src/main/java/com/example/springcloud/eurekafeignclient/EurekaFeignClientApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * @author admin
@@ -12,6 +14,8 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
 public class EurekaFeignClientApplication {
 
 	public static void main(String[] args) {
--- eureka-ribbon-client/src/main/java/com/example/springcloud/eurekaribbonclient/EurekaRibbonClientApplication.java
+++ eureka-ribbon-client/src/main/java/com/example/springcloud/eurekaribbonclient/EurekaRibbonClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * @author admin
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+@EnableHystrixDashboard
 public class EurekaRibbonClientApplication {
 
 	public static void main(String[] args) {
