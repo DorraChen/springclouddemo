4fd666cb95d0e7ec64411e928df2633dbead584a
clz <1814786186@qq.com>
2019-03-06 14:53:23 +0800

    turbine

--- eureka-monitor-client/src/main/java/com/example/springcloud/eurekamonitorclient/EurekaMonitorClientApplication.java
+++ eureka-monitor-client/src/main/java/com/example/springcloud/eurekamonitorclient/EurekaMonitorClientApplication.java
@@ -3,9 +3,15 @@ package com.example.springcloud.eurekamonitorclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+/**
+ * @author admin
+ * 2019-03-05 14:12:00
+ */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableTurbine
 public class EurekaMonitorClientApplication {
 
 	public static void main(String[] args) {
